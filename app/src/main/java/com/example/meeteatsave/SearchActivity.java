31a3c42d1a6ce7155d42c0e7c6a69bc7eeda6576
Amin <31611764+amin19948@users.noreply.github.com>
2019-10-23 20:55:20 +0200

    some design stuff

---
+++ app/src/main/java/com/example/meeteatsave/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.meeteatsave;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SearchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
